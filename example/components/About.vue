b7e7d244eb16c152ef7849c6d457ab291640df70
simonnozaki <tldk1993@gmail.com>
2024-08-15 09:00:58 +0900

    chore: Update examples

---
+++ example/components/About.vue
@@ -0,0 +1,12 @@
+<script lang="ts">
+import Message from '../components/atoms/Message.vue'
+
+export default {}
+</script>
+
+<template>
+  <div>
+    <h1>About</h1>
+    <Message message="This is an about page" />
+  </div>
+</template>
---
+++ example/components/atoms/Message.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+export default {
+  name: 'Message',
+  props: {
+    message: {
+      type: String,
+      required: true,
+    }
+  }
+}
+</script>
+
+<template>
+  <div>{{ message }}</div>
+</template>
