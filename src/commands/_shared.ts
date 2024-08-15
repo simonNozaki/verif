041b0fb62800da61271558c457bbc9e229457fb1
simonnozaki <tldk1993@gmail.com>
2024-08-15 20:44:08 +0900

    chore: add alias "f" for "format" option

--- src/commands/_shared.ts
+++ src/commands/_shared.ts
@@ -4,6 +4,7 @@ export const sharedArgs: Record<string, ArgDef> = {
   format: {
     type: 'string',
     default: 'graph',
+    alias: 'f',
     description: 'Output format. "graph" or "stdout", default is "graph"'
   }
 } as const
