c7bb26810a3d3f54bd0fd3d5131b8643aba3c633
simonNozaki <tldk1993@gmail.com>
2024-08-27 13:02:12 +0900

    refactor: Fetch data from not template but json by fetch (#14)
    * refactor: Fetch data from not template but json by fetch
    
    * chore: remove redundant variables

--- src/printers/graph-server/server.ts
+++ src/printers/graph-server/server.ts
@@ -5,44 +5,78 @@ import consola from 'consola'
 import { colorize } from 'consola/utils'
 import { toStaticPath } from '../../util'
 
-type ContentType = 'text/html' | 'application/javascript' | 'text/css'
+type ContentType = 'text/html' | 'application/javascript' | 'text/css' | 'application/json'
+
+interface PathRouter {
+  [k: string]: {
+    pattern: RegExp,
+    handler: (rawPath: string, res: HttpResponse) => void
+  }
+}
+
+/**
+ * Type alias of Node.js ServerResponse
+ */
+type HttpResponse = http.ServerResponse<http.IncomingMessage> & { req: http.IncomingMessage }
 
 export class GraphServer {
+  private readonly routes: PathRouter = {
+    index: {
+      pattern: new RegExp(/(^\/$|\/index.html)/),
+      handler: (_, res) => (this.writeStatic('index.html', 'text/html', res))
+    },
+    js: {
+      pattern: new RegExp(/cy.client.js/),
+      handler: (_, res) => (this.writeStatic('cy.client.js', 'application/javascript', res))
+    },
+    css: {
+      pattern: new RegExp(/\/*.css/),
+      handler: (rawPath, res) => {
+        const strs = rawPath.split('/')
+        const css = strs[strs.length - 1]
+        this.writeStatic(css, 'text/css', res)
+      }
+    },
+    json: {
+      pattern: new RegExp(/\/*.json/),
+      handler: (rawPath, res) => {
+        const strs = rawPath.split('/')
+        const json = strs[strs.length - 1]
+        this.writeStatic(json, 'application/json', res)
+      }
+    }
+  }
+
+  /**
+   * Find matching route and execute handler
+   * Ignore unknown paths
+   */
+  route (req: http.IncomingMessage, res: HttpResponse) {
+    for (const key of Object.keys(this.routes)) {
+      if(this.routes[key].pattern.test(req.url)) {
+        this.routes[key].handler(req.url, res)
+      }
+    }
+  }
+
   private _server = http.createServer((req, res) => {
-    if (req.url === '/' || req.url === '/index.html') {
-      this.writeStatic(
-        path.join(toStaticPath('index.html')),
-        'text/html',
-        res
-      )
-    } else if (req.url === '/cy.client.js') {
-      this.writeStatic(
-        path.join(toStaticPath('cy.client.js')),
-        'application/javascript',
-        res
-      )
-    }
-    else if (req.url?.match(/\/*.css/)) {
-      const strs = req.url.split('/')
-      const css = strs[strs.length - 1]
-      this.writeStatic(
-        path.join(toStaticPath(css)),
-        'text/css',
-        res
-      )
-    }
-    // Ignore unknown paths
+    this.route(req, res)
   })
 
   /**
    * Write static file by a path and content-type.
    */
   private writeStatic(fileName: string, contentType: ContentType, res: http.ServerResponse): void {
-    const source = fs.readFileSync(fileName, { encoding: 'utf-8' })
+    const filePath = path.join(toStaticPath(fileName))
+    fs.readFile(filePath, { encoding: 'utf-8' }, (err, data) => {
+      if (err) {
+        throw err
+      }
 
-    res.writeHead(200, { 'Content-Type': contentType })
-    res.write(source)
-    res.end()
+      res.writeHead(200, { 'Content-Type': contentType })
+      res.write(data)
+      res.end()
+    })
   }
 
   start(): void {
